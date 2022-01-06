53eade38335a1ac0dbc9ac39560d1dc9ef0c38cb
Kevin Childs <kevin.childs@revature.com>
2022-01-06 13:57:37 -0600

    testing jenkins

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,3 @@
-INSERT INTO accounts (username, password, firstname, lastname) VALUES ('automatedUsername', 'pass123', 'automatedFirstname', 'automatedLastname');
+INSERT INTO accounts (username, password, firstname, lastname) VALUES ('automatedUsername', 'pass123', 'Automated', 'User');
+INSERT INTO items (name, in_cart, account_id) VALUES ('milk', false, 1);
+INSERT INTO items (name, in_cart, account_id) VALUES ('cheese', true, 1);
